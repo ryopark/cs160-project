a5a5befefd501a41c216917a8f541d0f99dd87cc
Ryoji Park <dlbia1106@gmail.com>
2019-11-02 13:57:14 -0700

    Sign up without password

---
+++ src/components/AuthenticationForm.vue
@@ -0,0 +1,54 @@
+<template>
+  <form @submit.prevent="submit">
+    <input v-model="email" type="text" placeholder="SJSU email" data-email />
+    <button type="submit" :disabled="isSubmitting">submit</button>
+    <div v-if="errorMessage" class="errorMessage">{{ errorMessage }}</div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'AuthenticationForm',
+  data() {
+    return {
+      email: '',
+      errorMessage: null,
+      isSubmitting: false
+    }
+  },
+  methods: {
+    submit() {
+      if (/(\W|^)[\w.+-]*@sjsu\.edu(\W|$)/.test(this.email)) {
+        this.isSubmitting = true
+        this.$emit('submit', this.email)
+        this.isSubmitting = false
+      } else {
+        this.errorMessage = 'email address is not in a valid format. (i.e. exmaple@sjsu.edu)'
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  input {
+    padding: 0 10px;
+    margin-bottom: 10px;
+    width: 300px;
+    height: 30px;
+  }
+  button {
+    width: 300px;
+    height: 40px;
+    border-radius: 4px;
+    background: #2755ff;
+    color: #fff;
+    font-size: 14px;
+    font-weight: 700;
+  }
+}
+</style>
